d25aaac1515b53b7a6844ea16dca59ed9a664b8b
Maksym Tymchyk <maksym.tymchyk@gmail.com>
2019-04-09 19:10:24 +0300

    Yep

--- src/common/pages/no-match/no-match-text.ts
+++ src/common/pages/no-match/no-match-text.ts
@@ -4,4 +4,4 @@ export const wentWrong = __('Something went wrong...');
 export const cantFind = __('We can’t find the page you’re looking for.');
 export const letsAction = __('Check out our Help Center or Head back to home…');
 export const helpCenter = __('Help Center');
-export const backToHome = __('Back To Home');
+export const backToHome = __('Back Home');
