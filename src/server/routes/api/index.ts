480c60cf1cbdc480d8986fcc45e6acf018a7933a
Maksym Tymchyk <maksym.tymchyk@gmail.com>
2019-05-23 20:05:56 +0300

    Leave Feedback functinal is ready

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import emailSubscribe from './email-subscribe';
+import leaveFeedback from './leave-feedback';
 
 export default function createApiRouter(): express.Router {
     const apiRouter = express.Router();
 
     apiRouter.use(bodyParser.json());
     apiRouter.post('/email-subscribe', emailSubscribe);
+    apiRouter.post('/leave-feedback', leaveFeedback);
 
     return apiRouter;
 }
---
+++ src/server/routes/api/leave-feedback.ts
@@ -0,0 +1,56 @@
+import express from 'express';
+import logger from 'server/utils/logger';
+import config from 'server/config';
+import Telegraf from 'telegraf';
+
+const telegramBotToken = config.get<string>('app.telegramBotToken', undefined);
+const telegramChatId = config.get<string>('app.telegramChatID', undefined);
+
+export default async (req: express.Request, res: express.Response) => {
+    const { feedback, deviceId, appVersion, buildNumber, deviceBrand, country } = req.body;
+
+    logger.info(
+        `Message from: ${country} by ${appVersion} (${buildNumber}) / ${deviceId} / ${deviceBrand}`,
+    );
+
+    if (!telegramBotToken) {
+        res.sendStatus(204);
+        logger.error('No Telegram bot Token');
+
+        return;
+    }
+
+    if (!telegramChatId) {
+        res.sendStatus(204);
+        logger.error('No Telegram Chat ID');
+
+        return;
+    }
+
+    const telegraf = new Telegraf(telegramBotToken);
+
+    try {
+        let message = [
+            '<b>New Feedback from Plark Wallet</b>',
+            '',
+            feedback || '<i>No feedback 😨</i>',
+            '',
+            `<b>DeviceID:</b> ${deviceId}`,
+            `<b>Device:</b> ${deviceBrand}`,
+            `<b>App Version:</b> ${appVersion} (${buildNumber})`,
+            `<b>Country:</b> ${country}`,
+        ];
+
+        await telegraf.telegram.sendMessage(
+            telegramChatId,
+            message.join("\n"),
+            { parse_mode: 'HTML' },
+        );
+
+        logger.info(`Message sent to Chat`);
+    } catch (error) {
+        logger.error(error.message);
+    }
+
+    res.sendStatus(204);
+};
