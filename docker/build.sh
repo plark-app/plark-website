b3df4db0842acc4e994bdca2ea6213e3e9fbbc95
Maksym Tymchyk <m.tymchyk@p1k.co.uk>
2020-01-28 19:20:28 +0200

    Update docker helper bash script

--- docker/build.sh
+++ docker/build.sh
@@ -3,11 +3,49 @@
 NODE_ENV=production
 UN_ENV=production
 
-export APPLICATION_VERSION=$(jq -r ".version" package.json)
+##### Constants
+APPLICATION_VERSION=$(jq -r ".version" package.json)
+WITH_PUSH=0
 
-yarn locales:import
+for arg in "$@"
+do
+  case $arg in
+    --push*)
+      WITH_PUSH=1
+      shift
+      ;;
 
-docker build \
-    --file ./Dockerfile \
-    --tag plark/plark-website:$APPLICATION_VERSION \
-    --tag plark/plark-website:latest .
+    --version=*)
+      APPLICATION_VERSION="${arg#*=}"
+      shift
+      ;;
+  esac
+done
+
+build_docker()
+{
+    yarn locales:import
+
+    docker build \
+        --file ./Dockerfile \
+        --tag plark/plark-website:$APPLICATION_VERSION \
+        --tag plark/plark-website:latest .
+
+    # end of build_docker
+}
+
+push_docker()
+{
+    docker push plark/plark-website:$APPLICATION_VERSION
+    docker push plark/plark-website:latest
+    # end of push_docker
+}
+
+echo " ========= Start building ========= "
+build_docker
+
+if [[ $WITH_PUSH = "1" ]]
+then
+    echo " ========= Start pushing ========= "
+    push_docker
+fi
